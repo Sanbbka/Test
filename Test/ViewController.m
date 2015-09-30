8de6599bd0054fe7f7b968994aa7c21567a002a3
DenisovDenisov <denis.demchuk.96@mail.ru>
2015-09-30 17:01:49 +0300

    Initial Commit

---
+++ Test/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Test
+//
+//  Created by Denis on 30.09.15.
+//  Copyright (c) 2015 Alx. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Test
+//
+//  Created by Denis on 30.09.15.
+//  Copyright (c) 2015 Alx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
